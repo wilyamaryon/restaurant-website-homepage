eac25087f1232c315a6f90b7b9890d7900dccd31
William Oyouha <wilyamaryon@gmail.com>
2025-01-24 19:19:26 -0600

    Initial commit : Added restaurant website project

---
+++ includes/entete.php
@@ -0,0 +1,13 @@
+<div class="entete">
+    <img class="logo" src="images/logo.jpg" /> 
+	<p class="restfood"><span>G</span>ondwana<span> F</span>ood</p>
+    <span class="restaurant-acronym">Votre restaurant préféré...</span>
+    <nav class="menu">       
+        <ul >
+			<li><a href="#">Accueil</a></li>			
+			<li><a href="#">Menu</a></li>
+			<li><a href="#">Expert</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+    </nav>
+ </div>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <?php include("includes/entete.php") ?>
+    <header>
+        <h1>BEST QUALITY<br>FOOD</h1>
+        <div class="content">
+            <p class="motto">Plongez dans les saveurs exequises et des expériences cumlinaires inoubliables au Gondwana Food. Venez découvrir notre menu varié, préparé avec passion et expertise. Chaque plat est une invitation au voyage gustatif, vous promettant une expérience gastronomique mémorable à chaque bouchée.</p>
+            <button class='btn' type="button">Réserver ici</button>
+        </div>
+    </header>
+
+    <section class="temoignages">
+                <h2>Ce que nos clients pensent de nous</h2>
+                <div class="temoignage_card">
+                    <div class="card">
+                        <h5 class="name">Cindy M.</h5>
+                        <p>Nous nous y sommes rendu un vendredi soir en amoureux. Prix très accessibles pour un restaurant gastronomique</p>
+                    </div>
+                    <div class="card">
+                        <h5 class="name">Joëlle D.</h5>
+                        <p>Très bon, Le menu était délicieux, Les vins aussi !!!.</p>
+                    </div>
+                    <div class="card">
+                        <h5 class="name">Fabrice C</h5>
+                        <p>Wayyyyii , les gas vous avez tout dit !</p>
+                    </div>
+                    <div class="card">
+                        <h5 class="name">Romuald H.</h5>
+                        <p>Très bon, service rapide et super bon, rien à redire. </p>
+                    </div>
+                    <div class="card">
+                        <h5 class="name">Patricia H</h5>
+                        <p>Beau restaurant installé le long de l'eau. Personnel très professionnel et surtout cuisine raffinée.</p>
+                    </div>
+                    <div class="card">
+                        <h5 class="name">Serge G</h5>
+                        <p>Un petit lunch entre amis avec un délicieux poisson en plat, un vin blanc conseillé par le patron, à refaire dès que possible.....</p>
+                    </div>
+                </div>
+                <!--boutton de temoignages-->
+
+                
+                <!--ajouter page pour plus de temoignages--> 
+                <a href="views\temoignages_view.php"class="btn4">Plus de témoignages ICI</a>
+    </section>
+
+
+
+</body>
+</html>
